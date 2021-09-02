f9f1704b62fbc12cbcb44a82e0533f214429ee36
MagaMussetti <magamussetti@gmail.com>
2021-09-01 21:23:45 -0300

    actualizado navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,38 @@
-import "./Components/NavBar.css"
+import './NavBar.css';
 
-const NavBar = () => {​​​​​​​​
+const NavBar = () => {
     return (
-        <div className='container'>
-            <div className='logo-box'>
-                <img src='https://images.app.goo.gl/2kBkfX2yoEQh5PAZ6'/>
+        <div className="container">
+            <div className="logo-box">
+                <img src="" />
             </div>
-            <nav>
-                <ul>
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Productos</a></li>
-                    <li><a href="">Quienes Somos</a></li>
-                    <li><a href="">Contacto</a></li>
-                </ul>
+            <nav className="NavBar">
+                <div className="LeftNav">
+                    <div className="NavOptionsLeft">
+                        <ul>
+                            <li>
+                                <a href="">Inicio</a>
+                            </li>
+                            <li>
+                                <a href="">Productos</a>
+                            </li>
+                            <li>
+                                <a href="">Quienes Somos</a>
+                            </li>
+                            <li>
+                                <a href="">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="RightNav">
+                    <div className="NavOptionsRight">
+                        <button className="Option">SignIn</button>
+                        <button className="Option">SignUp</button>
+                    </div>
+                </div>
             </nav>
         </div>
-    )
-}​​​​​​​​
-export default NavBar
+    );
+};
+export default NavBar;
